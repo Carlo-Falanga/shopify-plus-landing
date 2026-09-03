609d4169d4b0fba8b1761fd28e5b19e04f6ec748
Carlo-Falanga <carlofalanga7@gmail.com>
2026-09-03 12:18:54 +0200

    fix: require a domain extension in store url

--- app/Http/Requests/StoreLeadRequest.php
+++ app/Http/Requests/StoreLeadRequest.php
@@ -9,6 +9,7 @@ use Illuminate\Validation\Rule;
 class StoreLeadRequest extends FormRequest
 {
     protected $redirect = '/#richiesta';
+
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -34,7 +35,7 @@ class StoreLeadRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:100'],
             'email' => ['required', 'email', 'max:255'],
-            'store_url' => ['required', 'url', 'max:255'],
+            'store_url' => ['required', 'url', 'regex:/^https?:\/\/[^\/]+\.[a-z]{2,}/i', 'max:255'],
             'current_platform' => ['required', Rule::in(['woocommerce', 'magento', 'prestashop', 'shopify', 'custom', 'other'])],
             'monthly_orders' => ['nullable', Rule::in(['0-500', '500-2000', '2000-10000', '10000+'])],
         ];
@@ -50,6 +51,7 @@ class StoreLeadRequest extends FormRequest
             'email.max' => "L'email è troppo lunga.",
             'store_url.required' => "Inserisci l'indirizzo del tuo negozio.",
             'store_url.url' => "L'indirizzo del negozio non sembra valido.",
+            'store_url.regex' => "L'indirizzo del negozio non sembra valido.",
             'store_url.max' => "L'indirizzo è troppo lungo.",
             'current_platform.required' => 'Seleziona la piattaforma che usi oggi.',
             'current_platform.in' => 'Seleziona una delle piattaforme proposte.',
