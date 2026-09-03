54a2eac07da7a7124458aacf04c2d00b904f47d9
Carlo-Falanga <carlofalanga7@gmail.com>
2026-09-03 17:35:49 +0200

    feat: load google tag manager container from env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @if (config('services.gtm.id'))
+        @include('partials.gtm-head')
+    @endif
     <title>@yield('title', 'Migrazione a Shopify Plus senza perdere SEO e ordini - Webgas')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -12,6 +15,9 @@
 </head>
 
 <body class="bg-white font-sans text-ink antialiased">
+    @if (config('services.gtm.id'))
+        @include('partials.gtm-body')
+    @endif
     @yield('content')
 </body>
 
